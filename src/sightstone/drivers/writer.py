8cf032f77ad8d9183b9184f264acd181236c3b04
Damian Owekro <damowerko@gmail.com>
2017-09-10 11:39:37 +0000

    raspberry pi

--- src/sightstone/drivers/writer.py
+++ src/sightstone/drivers/writer.py
@@ -1,35 +1,55 @@
+
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
+from time import sleep
+
 class Writer:
-    def __init__(self, DAT_A = 16, DAT_B = 18, CLK = 12):
+    def __init__(self, DAT = 8, CLK = 10, CLR = 12):
         # pins
-        self.DAT_A = DAT_A
-        self.DAT_B = DAT_B
+        self.DAT = DAT
         self.CLK = CLK
+        self.CLR = CLR
 
         # weather or not the driver is writing to the shift registers
         self.busy = False
-
-    def __enter__(self):
+    
+    def open(self):
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup([self.DAT_A, self.DAT_B, self.CLK], GPIO.OUT, initial=GPIO.LOW)
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
+        GPIO.setup([self.DAT, self.CLK], GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.CLR, GPIO.OUT, initial=GPIO.HIGH)
+    
+    def close(self):
         GPIO.cleanup()
 
-    def write_one(self, data = False):
-        GPIO.output([self.DAT_A, self.DAT_B], data)
-        GPIO.output(self.CLK, True)
-        GPIO.output(self.CLK, False)
+    def __enter__(self):
+        self.open()
+        return self
 
-    def write(self, data):
-        for d in data:
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def write_one(self, data = False):
+        if data or data == GPIO.HIGH:
+            data = GPIO.HIGH
+        else:
+            data = GPIO.LOW
+        GPIO.output(self.DAT, data)
+
+        sleep(0.000001)        
+        GPIO.output(self.CLK, GPIO.HIGH)
+        GPIO.output(self.CLK, GPIO.LOW)
+        sleep(0.000001)
+
+    def write(self, data_list):
+        data_list.reverse()
+        for data in data_list:
             self.write_one(data)
 
-
-
-
-
+    def clear(self):
+        print("Clear")
+        GPIO.output(self.CLR, GPIO.LOW)
+        sleep(0.000001)
+        GPIO.output(self.CLR, GPIO.HIGH)
